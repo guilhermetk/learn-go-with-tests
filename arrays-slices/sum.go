4b8735295a387e177be462db992e5a96cd1eaece
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-03-31 22:12:54 -0300

    feat: added array and slices

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -7,3 +7,24 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := []int{}
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTail(numbersToSumTail ...[]int) []int {
+	sums := []int{}
+	for _, numbers := range numbersToSumTail {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+
+}
--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysslices
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +16,34 @@ func TestSum(t *testing.T) {
 		}
 
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	assertEquals(t, got, want)
+}
+
+func TestSumAllTail(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		assertEquals(t, got, want)
+	})
+
+	t.Run("safely sum emtpy slices", func(t *testing.T) {
+		got := SumAllTail([]int{}, []int{})
+		want := []int{0, 0}
+
+		assertEquals(t, got, want)
+	})
+}
+
+func assertEquals(t testing.TB, got, want []int) {
+	t.Helper()
+	if !slices.Equal(got, want) {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
