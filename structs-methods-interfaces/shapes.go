ec8ef034ded564cb6c37c98f557effeccd05b6c8
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-03-31 22:30:27 -0300

    feat: added table driven tests

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -13,6 +13,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 type Circle struct {
 	Radious float64
 }
@@ -21,6 +25,11 @@ func (c Circle) Area() float64 {
 	return c.Radious * c.Radious
 }
 
-func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.Width + r.Height)
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
 }
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -18,15 +18,31 @@ func TestArea(t *testing.T) {
 	}
 
 	t.Run("test the area of a rectangle", func(t *testing.T) {
-		got := Rectangle{10.0, 10.0}
+		got := Rectangle{Width: 10.0, Height: 10.0}
 		want := 100.0
 		checkArea(t, got, want)
 	})
 	t.Run("test the area of a circle", func(t *testing.T) {
-		got := Circle{10.0}
+		got := Circle{Radious: 10.0}
 		want := 100.0
 		checkArea(t, got, want)
 	})
+}
+
+func TestAreaTableDriven(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radious: 10}, want: 100.0},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		assertEquals(t, got, tt.want)
+	}
 }
 
 func assertEquals(t testing.TB, got, want float64) {
